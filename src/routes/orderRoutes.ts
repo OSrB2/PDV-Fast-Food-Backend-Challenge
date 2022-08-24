acc34b884d068e818593ef76aa98197f2a018f01
Pedro <pedro_oliva@oulook.com.br>
2022-08-24 10:47:32 -0300

    feat: adding validations to routes

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { OrderController } from "../domain/order/controllers/orderControllers";
+import { orderValidation } from "../domain/order/validation";
+
 
 const orderRoutes = express.Router();
 
-orderRoutes.post("/order", OrderController.create);
-orderRoutes.get("/orders", OrderController.getAll);
-orderRoutes.get("/order/:id", OrderController.getByID);
-orderRoutes.put("/order/update/:id", OrderController.update);
-orderRoutes.patch("/order/close/:id", OrderController.close);
+orderRoutes.post("/order", orderValidation.create, OrderController.create);
+orderRoutes.get("/orders",OrderController.getAll);
+orderRoutes.get("/order/:id",OrderController.getByID);
+orderRoutes.put("/order/update/:id",orderValidation.update, OrderController.update);
+orderRoutes.patch("/order/close/:id",orderValidation.close, OrderController.close);
 
 export default orderRoutes;
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { ProductController } from "../domain/product/controllers/productController";
+import { prodValidation } from "../domain/product/validation"
+
 
 const prodRoutes = express.Router();
 
-prodRoutes.post("/products", ProductController.create);
+prodRoutes.post("/products",prodValidation.create, ProductController.create);
 prodRoutes.get("/products", ProductController.getAll);
 prodRoutes.get("/products/:id", ProductController.getByID);
 prodRoutes.get("/product/:name", ProductController.prodByName);
-prodRoutes.put("/product/update/:id", ProductController.update);
-prodRoutes.patch("/product/disable/:id", ProductController.disable);
+prodRoutes.put("/product/update/:id", prodValidation.update, ProductController.update);
+prodRoutes.patch("/product/disable/:id", prodValidation.disable, ProductController.disable);
 
 export default prodRoutes;
